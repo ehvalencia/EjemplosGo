628ce0d9703f62c4f5f55ca2eefb14c2346aead0
ehvalencia <edwin.valencia@mercadolibre.com.co>
2023-03-28 12:24:50 -0500

    ejercicios de clase 3 ok

--- dia3/ejercicio1/ejercicio1.go
+++ dia3/ejercicio1/ejercicio1.go
@@ -1,12 +1,12 @@
 /*
 Crear un programa que cumpla los siguiente puntos:
-Tener una estructura llamada Product con los campos ID, Name, Price, Description y Category.   
+Tener una estructura llamada Product con los campos ID, Name, Price, Description y Category.
 Tener un slice global de Product llamado Products instanciado con valores.
-2 métodos asociados a la estructura Product: Save(), GetAll(). El método Save() deberá tomar el slice de Products y 
-añadir el producto desde el cual se llama al método. El método GetAll() deberá imprimir todos los productos guardados 
+2 métodos asociados a la estructura Product: Save(), GetAll(). El método Save() deberá tomar el slice de Products y
+añadir el producto desde el cual se llama al método. El método GetAll() deberá imprimir todos los productos guardados
 en el slice Products.
-Una función getById() al cual se le deberá pasar un INT como parámetro y retorna el producto correspondiente 
-al parámetro pasado. 
+Una función getById() al cual se le deberá pasar un INT como parámetro y retorna el producto correspondiente
+al parámetro pasado.
 Ejecutar al menos una vez cada método y función definido desde main().
 */
 package main
@@ -14,38 +14,40 @@ package main
 import "fmt"
 
 type Product struct {
-	ID 				int
-	Name			string
-	Price			float32
-	Description 	string
-	Category		string
+	ID          int
+	Name        string
+	Price       float32
+	Description string
+	Category    string
 }
 
 var Products = []Product{
-	{1,"Teclado", 600.000,"Lenovo", "Tecnologia" }
+	{1, "Teclado", 600.000, "Lenovo", "Tecnologia"},
 }
 
-func (p Product) Save() []Product{
-	var NewP = append (Products, p)
+func (p Product) Save() []Product {
+	var newP = append(Products, p)
+	return newP
 }
 
-func (p Product) GetAll (){
+func (p Product) GetAll() {
 	fmt.Println(Products)
 }
 
-func getById (id int)Product {
-	for _, := range Products
-		if p.ID == id{
+func getById(id int) Product {
+	for _, p := range Products {
+		if p.ID == id {
 			return p
 		}
- return Product{}		
+	}
+
+	return Product{}
 }
 
-func main (){
-	p := Product{2,"Mause", 200.000,"Lenovo", "Tecnologia" }
-	Product = p.Save()
+func main() {
+	p := Product{2, "Mouse", 200.000, "Lenovo", "Tecnologia"}
+	Products = p.Save()
 	p.GetAll()
-	fmt.Println("Productos "+ getById(1))
-	fmt.Println("Productos "+ getById(2))
+	fmt.Println(getById(1))
+	fmt.Println(getById(2))
 }
-
