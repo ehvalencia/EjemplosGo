580d76abdd83c928c39ab331926f80af78c06e70
ehvalencia <edwin.valencia@mercadolibre.com.co>
2023-04-05 11:43:00 -0500

    api

---
+++ goWeb/Api/cmd/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/gin-gonic/gin"
+
+	handler "github.com/ehvalencia/EjemplosGo/EjemplosGo/goWeb/Api/cmd/handlers"
+	"github.com/ehvalencia/EjemplosGo/EjemplosGo/goWeb/Api/internal/domain"
+	"github.com/ehvalencia/EjemplosGo/EjemplosGo/goWeb/Api/internal/products"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load("/Users/edvalencia/Documents/GO/EjemplosGo/goWeb/Api/token.env")
+	if err != nil {
+		panic(err)
+	}
+
+	var productsList = []domain.Product{}
+	loadProducts("/Users/edvalencia/Documents/GO/EjemplosGo/goWeb/Api/products.json", &productsList)
+
+	repo := products.NewRepository(productsList)
+	service := products.NewService(repo)
+	productHandler := handler.NewProductHandler(service)
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) { c.String(200, "pong") })
+
+	products := r.Group("/products")
+	{
+		products.GET("", productHandler.GetAll())
+		products.GET(":id", productHandler.GetByID())
+		products.GET("/search", productHandler.Search())
+		products.POST("", productHandler.Post())
+		products.DELETE(":id", productHandler.Delete())
+		products.PATCH(":id", productHandler.Patch())
+
+	}
+	r.Run(":8080")
+}
+
+// loadProducts carga los productos desde un archivo json
+func loadProducts(path string, list *[]domain.Product) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal([]byte(file), &list)
+	if err != nil {
+		panic(err)
+	}
+}
